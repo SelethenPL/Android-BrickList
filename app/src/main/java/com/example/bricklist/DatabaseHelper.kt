71001c281ce84d477eb384a7c95329cfb5c1219d
SelethenPL <lukisd@op.pl>
2020-05-28 18:59:51 +0200

    Changed database management.

---
+++ app/src/main/java/com/example/bricklist/DatabaseHelper.kt
@@ -0,0 +1,14 @@
+package com.example.bricklist
+
+import android.content.Context
+import com.readystatesoftware.sqliteasset.SQLiteAssetHelper
+
+class DatabaseHelper (context: Context)
+    : SQLiteAssetHelper(context, DATABASE_NAME, null, DATABASE_VERSION) {
+
+    companion object {
+        private const val DATABASE_NAME = "BrickList.db"
+        private const val DATABASE_VERSION = 1
+    }
+
+}
--- app/src/main/java/com/example/bricklist/MainActivity.kt
+++ app/src/main/java/com/example/bricklist/MainActivity.kt
@@ -1,28 +1,24 @@
 package com.example.bricklist
 
-import android.os.AsyncTask
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import kotlinx.android.synthetic.main.activity_main.*
-import org.w3c.dom.Document
-import org.w3c.dom.Element
-import org.w3c.dom.Node
-import org.w3c.dom.NodeList
-import java.io.File
-import java.io.FileNotFoundException
-import java.io.FileOutputStream
-import java.io.IOException
-import java.net.MalformedURLException
-import java.net.URL
-import javax.xml.parsers.DocumentBuilderFactory
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun queryOnClick(v: View) {
+        val databaseAccess = DatabaseAccess(this).getInstance(applicationContext)
+        databaseAccess?.open()
+
+        result.text = databaseAccess?.getColors()
+
+        databaseAccess?.close()
     }
 
 }
